87374da4468c03d1dda89e2c65558c382172ec10
Ryan-DePrekel <ryan.j.deprekel@wmich.edu>
2016-09-19 15:26:00 -0400

    added some tests for hashmap

--- src/hashmap/test.c
+++ src/hashmap/test.c
@@ -9,6 +9,14 @@ int main(void) {
 
     hashmap_set(hashmap, "ab", "ab");
     hashmap_set(hashmap, "ba", "ba");
+    hashmap_set(hashmap, "123", "123");
+    hashmap_set(hashmap, "This Is A String With Spaces", "This Is A String With Spaces");
+    hashmap_set(hashmap, "String with spaces and 1332", "String with spaces and 1332");
+    hashmap_set(hashmap, "","");
     assert(strcmp(hashmap_get(hashmap, "ab"), "ab") == 0);
     assert(strcmp(hashmap_get(hashmap, "ba"), "ba") == 0);
+    assert(strcmp(hashmap_get(hashmap, "123"), "123") == 0);
+    assert(strcmp(hashmap_get(hashmap, "This Is A String With Spaces"), "This Is A String With Spaces") == 0);
+    assert(strcmp(hashmap_get(hashmap, "String with spaces and 1332"), "String with spaces and 1332") == 0);
+    assert(strcmp(hashmap_get(hashmap, ""), "") == 0);
 }
